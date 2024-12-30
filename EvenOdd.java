a5aca659d9f7932c0dbf8456fc50bc8c8ac1069e
Shadab Akhtar <0112cs211108@gmail.com>
2024-12-30 19:01:49 +0530

    Java

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+//WAP to check for even or odd number
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int no = 90;
+        if (no % 2 == 0) {
+            System.out.println("no is even");
+        }
+        else{
+            System.out.println("no is odd");
+        }
+    }
+    
+}
