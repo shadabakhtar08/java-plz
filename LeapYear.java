649f2f5a4e10b61b44c58485cf91fff32bda9065
Shadab Akhtar <0112cs211108@gmail.com>
2024-12-30 19:44:32 +0530

    Java

--- LeapYear.java
+++ LeapYear.java
@@ -2,7 +2,7 @@
 
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 2017;
+        int year = 2016;
         if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
             System.out.println("Leap Year");            
         }
