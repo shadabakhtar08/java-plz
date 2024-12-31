34c2ab7db4fb303fec3ea16ee15806e0fefc0550
Shadab Akhtar <0112cs211108@gmail.com>
2025-01-01 04:18:44 +0530

    Java

---
+++ UserInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = s.next();
+        System.out.println("Enter your Age : ");
+        int age = s.nextInt();
+        System.out.println("Enter your Gender : ");
+        char gender = s.next().charAt(0);
+        System.out.println("Enter your Phone No : ");
+        long phno = s.nextLong();
+        System.out.println("-------------------------------------------");
+        System.out.println("Name :"+name);
+        System.out.println("Age :"+age);
+        System.out.println("Gender :"+gender);
+        System.out.println("Phone no :"+phno);
+    }
+}
