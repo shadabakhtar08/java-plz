1667cd67c3c2775d930f32bd171798c8c0505ec0
Shadab Akhtar <0112cs211108@gmail.com>
2025-01-01 19:10:33 +0530

    Java

--- Calculator.java
+++ Calculator.java
@@ -2,37 +2,42 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter First No. : ");
-        int no1 = s.nextInt();
-        System.out.println("Enter Second No. : ");
-        int no2 = s.nextInt();
-        System.out.println("Select  Symbol (+,-,*,/)");
-        String sym = s.next();
-        int res;
-        switch (sym) {
-            case "+":
-                res = no1 + no2;
-                System.out.println("Addition is : " + res);
-                break;
-            case "-":
-                res = no1 - no2;
-                System.out.println("Subtration is : " + res);
-                break;
-            case "*":
-                res = no1 * no2;
-                System.out.println("Multiplication is : " + res);
-                break;
-            case "/":
-                res = no1 / no2;
-                System.out.println("Division is : " + res);
-                break;
+        String yn;
+        do {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter First No. : ");
+            int no1 = s.nextInt();
+            System.out.println("Enter Second No. : ");
+            int no2 = s.nextInt();
+            System.out.println("Select  Symbol (+,-,*,/)");
+            String sym = s.next();
+            int res;
+            switch (sym) {
+                case "+":
+                    res = no1 + no2;
+                    System.out.println("Addition is : " + res);
+                    break;
+                case "-":
+                    res = no1 - no2;
+                    System.out.println("Subtration is : " + res);
+                    break;
+                case "*":
+                    res = no1 * no2;
+                    System.out.println("Multiplication is : " + res);
+                    break;
+                case "/":
+                    res = no1 / no2;
+                    System.out.println("Division is : " + res);
+                    break;
 
-            default:
-                System.out.println("Invalid Symbol");
-                break;
+                default:
+                    System.out.println("Invalid Symbol");
+                    break;
 
             }
-            s.close();
+
+            System.out.println("Do you want to continue (Press y for Yes and n for No)");
+            yn = s.next();
+        } while (yn.equals("y") || yn.equals("Y"));
     }
 }
